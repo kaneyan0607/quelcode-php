8d4328ff118f3d714a189023b6c8b1fdf985805f
kaneyan0607 <kaneyan0607@gmail.com>
2020-05-08 10:26:29 +0900

    secound

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,12 +3,11 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-    $target = count($array)-$i-1;
-    for($j = 0; $j < $target; $j++){
-         if($array[$j] > $array[$j+1]){
-            $swap = $array[$j];
-            $array[$j] = $array[$j+1];
-            $array[$j+1] = $swap;
+    for($j = $i; $j < count($array); $j++) {
+        if ($array[$i] >= $array[$j]) {
+            $tmp = $array[$i];
+            $array[$i] = $array[$j];
+            $array[$j] = $tmp;
         }
     }
 }
