7631f84943b7b61219457fb43072184928e7d254
kaneyan0607 <kaneyan0607@gmail.com>
2020-05-08 14:03:27 +0900

    5th

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,7 +4,6 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 $length = count($array);
 for ($i = 0; $i < $length; $i++) {
-    $length = count($array);
     for($j = $i + 1; $j < $length; $j++) {
         if ($array[$i] > $array[$j]) {
             $tmp = $array[$i];
